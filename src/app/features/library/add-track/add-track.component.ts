f8ce0f9c2feeeb9d9864904186d56e9906bfa222
amdymstfa <amdymoustapha011@gmail.com>
2026-01-08 15:30:02 +0100

    Feat: add add-track component, config route's

---
+++ src/app/features/library/add-track/add-track.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-track',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-track.component.html',
+  styleUrl: './add-track.component.css'
+})
+export class AddTrackComponent {
+
+}
---
+++ src/app/features/library/add-track/add-track.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { AddTrackComponent } from "./add-track.component";
+
+export const ADD_TRACK_ROUTES: Routes = [
+
+    {
+        path:'',
+        component: AddTrackComponent
+    }
+];
