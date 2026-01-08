4a6e32a23d6a39a1e5a7a31faa6250c288722f4d
amdymstfa <amdymoustapha011@gmail.com>
2026-01-08 14:27:26 +0100

    Set config of track component

---
+++ src/app/features/track-detail/track-detail.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { TrackDetailComponent } from "./track-detail.component";
+
+export const TRACK_DETAIL_ROUTES: Routes = [
+
+    {
+        path:'',
+        component: TrackDetailComponent
+    }
+];
