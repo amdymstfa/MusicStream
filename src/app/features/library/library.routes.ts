05faa65f30998d962a4c4a69705db6e4a15a70bc
amdymstfa <amdymoustapha011@gmail.com>
2026-01-08 11:40:20 +0100

    Config : route of library component

---
+++ src/app/features/library/library.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from "@angular/router";
+import { LibraryComponent } from "./library.component";
+
+export const LIBRARY_ROUTES: Routes = [
+
+    {
+        path: '',
+        component: LibraryComponent 
+    }
+];
