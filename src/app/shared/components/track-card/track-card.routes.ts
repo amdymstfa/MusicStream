af890aa37b3a1877270eb1a7ccb5948abc0004bb
amdymstfa <amdymoustapha011@gmail.com>
2026-01-08 15:31:15 +0100

    Feat: add reusable track-card component, config route's

---
+++ src/app/shared/components/track-card/track-card.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { TrackCardComponent } from "./track-card.component";
+
+export const TRACK_CARD_ROUTES: Routes = [
+    {
+        path: '',
+        component: TrackCardComponent
+    }
+];
