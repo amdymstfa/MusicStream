0f9e620a085a428485f52a3c4a12743d33d673ed
amdymstfa <amdymoustapha011@gmail.com>
2026-01-08 11:57:54 +0100

    -redirect home page of application -redierct to default page when page is not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 
-    
+    // default page of this app
+   {
+        path: '',
+        redirectTo: 'library',
+        pathMatch: 'full'
+   },
+
+   // default page in not found cas
+   {
+        path: '**',
+        redirectTo: 'library'
+   }
 ];
