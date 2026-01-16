3db64bc64ae2a9e4f7a3e9d038c174a7aee61722
amdymstfa <amdymoustapha011@gmail.com>
2026-01-16 10:56:14 +0100

    feat: handle track on localstorage

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Track } from '../models/track.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,19 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  private readonly STORAGE_KEY = 'musicstream_tracks';
+
+  getTracks() : Track[] {
+      const data = localStorage.getItem(this.STORAGE_KEY);
+      return data ? JSON.parse(data) : [] ;
+  }
+
+  saveTracks( tracks : Track) : void {
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(tracks));
+  }
+
+  clear() : void {
+    localStorage.removeItem(this.STORAGE_KEY);
+  }
 }
