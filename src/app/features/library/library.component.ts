cf573397af01315c47f11341378f2457d94d8fb4
amdymstfa <amdymoustapha011@gmail.com>
2026-01-17 18:40:48 +0100

    feat: add a standalone libraryComponent to display tracks from service

--- src/app/features/library/library.component.ts
+++ src/app/features/library/library.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Track } from '../../core/models/track.model';
+import { TrackService } from '../../core/services/track.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit{
 
+    tracks: Track[] = [];
+    loading = false ;
+    error: string | null = null ;
+
+    constructor(private trackService: TrackService){}
+  
+    ngOnInit(): void {
+
+      // load tracks
+      this.trackService.loadTracks();
+
+      // listen tracks
+      this.trackService.tracks$.subscribe(tracks => {this.tracks = tracks});
+
+      // listen loading
+      this.trackService.loading$.subscribe(loading => {this.loading = loading});
+
+      // listen error 
+      this.trackService.error$.subscribe(error => {this.error = error});
+        
+    }
 }
